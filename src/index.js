a6710bd62d8e349a4c96f2b0aa5875301360b1fe
Ricardo Maya <rickmaya@gmail.com>
2020-11-25 15:07:43 -0600

    add calculateWinner function to determine winner and utilizein Boards render function

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 //function component instead of stateless class
 function Square(props) {
   return (
-    <button onclick={props.onClick}>
+    <button onClick={props.onClick}>
       {props.value}
     </button>
   )
@@ -38,7 +38,9 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     
     return (
       <div>
@@ -81,6 +83,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[b] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
