436a179e97f4006ec4836ead2fe8cd708e602f42
Ricardo Maya <rickmaya@gmail.com>
2020-11-25 14:13:14 -0600

    fixed code grammer errors

--- src/index.js
+++ src/index.js
@@ -22,8 +22,10 @@ class Board extends React.Component {
   handleClick(i) {
     const squares = this.state.squares.slice();
     squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({squares: squares});
-    xIsNext: !this.state.xIsNext,
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
